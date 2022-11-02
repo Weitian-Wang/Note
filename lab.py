d76d8c9396e1282b71be40485c4cea18155e1023
wangweitian <wwt624@126.com>
2022-11-01 18:41:05 -0700

    Update 250P note to Cache

--- lab.py
+++ lab.py
@@ -1,13 +1,3 @@
-class Node:
-    def __init__(self, data = 0, left = None, right = None):
-        self.data = data
-        self.left = left
-        self.right = right
-
-def create_tree(list, l_idx, r_idx):
-    if l_idx == r_idx:
-        pass
-    
 def main():
     list = [1,2,3,4,5,99]
 
